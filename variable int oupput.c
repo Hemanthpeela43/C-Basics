3f1338f0938a1c557991d3c458109a61c9bdca31
Hemanthpeela43 <92840398+Hemanthpeela43@users.noreply.github.com>
2021-10-20 11:01:13 +0530

    Add files via upload

---
+++ variable int oupput.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable decleration
+	int age;
+	double cgpa;
+	char initial;
+	//initialization
+	age=18;
+	cgpa=8.9;
+	initial ='B';
+	//output
+	printf("age of the student is %d\n",age);
+	printf(" cgpa of the student is %lf\n",cgpa);
+	printf(" student surname is %c\n",initial);
+}
