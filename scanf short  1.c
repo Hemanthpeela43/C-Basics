98a7998f4036a5e508dac48155a6f6071f649c27
Hemanthpeela43 <92840398+Hemanthpeela43@users.noreply.github.com>
2021-11-09 10:33:32 +0530

    Add files via upload

---
+++ scanf short  1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter three numbers");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("number1: %d",a);
+	printf("\n number2: %d",b);
+	printf("\n number3: %d",c);
+}
