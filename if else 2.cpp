955c4a662c770b3cad18d6ffcc1800eadee32096
Hemanthpeela43 <92840398+Hemanthpeela43@users.noreply.github.com>
2021-12-06 10:44:28 +0530

    Add files via upload

---
+++ if else 2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number to check  whether it is positive or negitive");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("%d is positive numbeer",n);
+	}
+	else
+	{
+		printf("%d is negitive numbeer",n);
+	}
+}
